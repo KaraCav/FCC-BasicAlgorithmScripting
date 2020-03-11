c4d483af08980dd335c847e0c9af7cefbcec160a
Kara Cavanaugh <karacav@msn.com>
2020-03-11 16:48:53 -0400

    Number 1

--- Pt2.js
+++ Pt2.js
@@ -1,1 +1,11 @@
-// 1. Return Largest Numbers in Arrays
+// 1. Return true if a value is a boolean primitive
+function booWho(bool) {
+  if(bool === true || bool === false){
+    return true;
+  }
+  else {
+  return false;
+  }
+}
+
+booWho(null);

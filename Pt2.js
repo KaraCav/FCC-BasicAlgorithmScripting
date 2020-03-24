bdfbb78ab47381e283259a3b7899da892f4e13d6
Kara Cavanaugh <karacav@msn.com>
2020-03-23 20:38:22 -0400

    Attempt Number 12

--- Pt2.js
+++ Pt2.js
@@ -1,4 +1,4 @@
-// 1. Return true if a value is a boolean primitive
+// 11. Return true if a value is a boolean primitive
 function booWho(bool) {
   if(bool === true || bool === false){
     return true;
@@ -9,3 +9,26 @@ function booWho(bool) {
 }
 
 booWho(null);
+
+// 12. Title Case a Sentence
+//Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+// FIRST ATTEMPT
+function titleCase(str) {
+  let lowerString = str.toLowerCase();
+  let newString = "";
+  console.log(lowerString.length);
+  for(let i = 0; i < lowerString.length; i++) {
+    if(lowerString[i] == " ") {
+      let upperCaseLetter = lowerString[i + 1].toUpperCase();
+    newString = newString + " " + upperCaseLetter;
+    }
+    else {
+      newString = newString + lowerString[i];
+    }
+    console.log(newString);
+  }
+console.log(typeof(newString));
+  return str;
+}
+
+titleCase("I'm a little tea pot");

1a606cce2c13258a48ba6dec4d5075a99011aa12
Kara Cavanaugh <karacav@msn.com>
2021-08-03 20:23:42 -0700

    Number 17- Array updating

--- Pt2.js
+++ Pt2.js
@@ -109,3 +109,18 @@ function bouncer(arr) {
   return arr.filter(Boolean); 
 }
 // Explanation: The Array.prototype.filter method expects a function that returns a Boolean value which takes a single argument 
+
+// 17. Where do I Belong- Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. 
+// The returned value should be a number. EX: getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] >= num) {
+      console.log(arr[i - 1])
+      return i;
+    }
+  }
+  return arr.length;
+}

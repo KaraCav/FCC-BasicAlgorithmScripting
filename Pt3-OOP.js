ef132a1df506c0175e01bc76dcdde714a5ba706c
Kara C <karacav@msn.com>
2021-08-10 19:44:35 -0700

    Number 3-5: Constructor Functions

--- Pt3-OOP.js
+++ Pt3-OOP.js
@@ -11,3 +11,25 @@ let dog = {
   numLegs: 4,
   sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
 };
+
+// 3. Define a Constructor Function
+function Dog() {
+  this.name = "Fido",
+  this.numLegs = 4
+}
+
+// 3B. Create a new Object
+let hound = new Dog();
+
+// 4. Create a Constructor that Accepts Parameters
+function Dog(name, numLegs) {
+  this.name = name,
+  this.numLegs = numlegs
+}
+
+// 5. Verify an Object's Constructor with instanceof
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+let myHouse = new House(4);
+myHouse instanceof House; // true

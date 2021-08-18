9ce8776da67de08684e0b2f93b0c673bc47e3b10
Kara C <karacav@msn.com>
2021-08-18 00:42:28 -0400

    Number 8- iterating over props

--- Pt3-OOP.js
+++ Pt3-OOP.js
@@ -54,3 +54,22 @@ function Dog(name) {
   this.name = name;
 }
 Dog.prototype.numLegs = 4;
+
+// 8. Iterate Over All Properties
+function Dog(name) {
+  this.name = name;
+}
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+let ownProps = [];
+let prototypeProps = [];
+
+for(let prop in beagle) {
+  if(beagle.hasOwnProperty(prop)) {
+    ownProps.push(prop);
+  }
+  else {
+    prototypeProps.push(prop);
+  }
+}

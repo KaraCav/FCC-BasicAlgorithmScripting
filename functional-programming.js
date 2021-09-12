ec18e0da67e1a1373b230a4f36459e99dd633799
Kara C <karacav@msn.com>
2021-09-11 17:41:55 -0700

    Number 3- Closing Window Tabs

--- functional-programming.js
+++ functional-programming.js
@@ -30,3 +30,33 @@ const getTea = (prepareTea, numOfCups) => {
 
 const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
 const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+
+// 3.Understand the Hazards of Using Imperative Code- Make changes to the Window object to add and remove tabs
+var Window = function(tabs) {
+  this.tabs = tabs; // record of the array in the object
+};
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab'); 
+  return this;
+};
+Window.prototype.tabClose = function (index) {
+  var tabsBeforeIndex = this.tabs.slice(0, index);
+  var tabsAfterIndex = this.tabs.slice(index + 1); 
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex);
+console.log("my 1st answer",this.tabs);
+  return this;
+ };
+
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); 
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); 
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); 
+
+var finalTabs = socialWindow
+  .tabOpen() // Open a new tab 
+  .join(videoWindow.tabClose(2)) // Close 3rd tab in window and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log("final", finalTabs.tabs);

43abac5f0ac665fd26b3ab9261bc2e17191e50b2
Kara C <karacav@msn.com>
2021-10-12 20:29:05 -0700

    Number 9- Filter on a Prototype

--- functional-programming.js
+++ functional-programming.js
@@ -107,3 +107,22 @@ var filteredList = watchList.map(function(movie) {
     return { title: movie["Title"], rating: movie["imdbRating"] };
   })
   .filter(movie => movie.rating >= 8);
+
+// 9. Implement the filter Method on a Prototype
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  var newArray = [];
+
+ for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
+      console.log(newArray);
+    }
+  }
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
